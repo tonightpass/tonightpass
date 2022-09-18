9d7d0f50d9bf02a6797744d878ba3b40ec0ddc12
Antoine Kingue <etorefr@gmail.com>
2022-09-18 22:59:26 +0200

    🏷️ add organization and event types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
+// User
 export type User = {
   id: string;
   identifier: UserIdentifier;
   password: string;
   identity: UserIdentity;
   tokens: UserToken[];
-  addresses: UserAddress[];
+  addresses: Address[];
   preferences: UserPreferences;
   connections: UserConnection[];
   updatedAt: Date;
@@ -37,15 +38,7 @@ export type UserToken = {
 
 export type UserTokenType = "recovery" | "email" | "phone";
 
-export type UserAddress = {
-  id: string;
-  street: string;
-  zipCode: string;
-  city: string;
-  country: string; // was missing
-};
-
-export type UserPreferences = undefined; // missing type
+export type UserPreferences = unknown; // missing type
 
 export type UserConnection = {
   ip: string;
@@ -69,4 +62,93 @@ export type UserConnectionDevice = {
 export type UserConnectionClient = {
   name: string;
   version: string;
+};
+
+// Organization
+export type Organization = {
+  id: string;
+  name: string;
+  socials: OrganizationSocial[];
+  members: OrganizationMember[];
+  adress: Address;
+  tickets: EventTicket[];
+  events: Event[];
+  updatedAt: Date;
+  createdAt: Date;
+};
+
+export type OrganizationSocial = {
+  type: OrganizationSocialType;
+  url: string;
+};
+
+export type OrganizationSocialType = "facebook" | "twitter" | "instagram" | "linkedin" | "youtube" | "website" | string;
+
+export type OrganizationMember = {
+  email: string;
+  role: OrganizationMemberRole;
+};
+
+export type OrganizationMemberRole = unknown; // missing type
+
+// Event
+export type Event = {
+  title: string;
+  description: string;
+  slug: string;
+  organization: Organization;
+  type: EventType;
+  public: boolean;
+  flyers: string[];
+  trailers: string[];
+  location: Address;
+  tickets: EventTicket[];
+  startAt: Date;
+  endAt: Date;
+  updatedAt: Date;
+  createdAt: Date;
+};
+
+export enum EventType {
+  "clubbing",
+  "concert",
+  "afterwork",
+  "dancing_lunch",
+  "diner",
+  "garden",
+  "after_beach",
+  "festival",
+  "spectacle",
+  "cruise",
+  "outside_animation",
+  "sport",
+  "match",
+  "seminar",
+  "conference",
+  "wellness_day",
+  "workshop",
+  "trade_fair",
+  "consumer_show",
+  "membership"
+}
+
+export type EventTicket = {
+  id: string;
+  title: string;
+  price: number;
+  fee: boolean;
+  quantity: number;
+  startAt: Date;
+  endAt: Date;
+  updatedAt: Date;
+  createdAt: Date;
+};
+
+// Address
+export type Address = {
+  id: string;
+  street: string;
+  zipCode: string;
+  city: string;
+  country: string; // was missing
 };
