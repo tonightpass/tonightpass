2031b2f66eb7bf49f4edc5f7a8229bc5a8fefd2d
Gaurav Malviya <gaurav-malviya@outlook.com>
2022-09-24 21:13:07 +0530

    Add files via upload
    I went through the discord channel and saw some functionality which it has. Based on that  I have added two parameter on top of the recommended parameter.  They are:
    1)muteChannel--> which takes enum (duration of mute) as an input
    2)inviteLink--> i thought it will be cool if there is an invite link for the channel type

---
+++ channel/index.d.ts
@@ -0,0 +1,21 @@
+import {User} from "../user";
+
+export type channel =
+{
+    id         ?: string;
+    type       ?: "private"|"group";
+    user        : User[];
+    createdOn  ?: Date;
+    muteChannel : duration; //mute Channel which will accept number b
+    invinteLink : string; //Channel invinte link
+}
+
+export enum duration
+{
+    fifteenMinute = 15,
+    oneHour       = 1,
+    threeHour     = 3,
+    eightHour     = 8,
+    twentyFourHour= 24,
+    manualTurnOn  = 1
+}
