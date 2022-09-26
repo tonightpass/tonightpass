2e7d01cbcb2daa12a56f4e7cccf7aa34212674b3
Gaurav Malviya <gaurav-malviya@outlook.com>
2022-09-26 13:09:59 +0530

    Update index.d.ts

--- src/channel/index.d.ts
+++ src/channel/index.d.ts
@@ -12,14 +12,6 @@ export type channel =
 
 export enum duration
 {
-    // fifteenMinute = 15,
-    // oneHour       = 10,
-    // threeHour     = 30,
-    // eightHour     = 80,
-    // twentyFourHour= 240,
-    // manualTurnOn  = 1,
-    // manualTurnOff = 0
-
     fifteenMinute = 0.25,
     oneHour       = 60,
     threeHour     = 180,
