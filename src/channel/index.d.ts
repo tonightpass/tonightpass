feb317545a1ca7c571e5aa94347f5b4812082eb3
Gaurav Malviya <gaurav-malviya@outlook.com>
2022-09-26 15:48:10 +0530

    Update index.d.ts

--- src/channel/index.d.ts
+++ src/channel/index.d.ts
@@ -6,15 +6,15 @@ export type Channel = {
   user: User[];
   createdOn?: Date;
   muteChannel: Duration; //mute Channel which will accept number from enum (duration)
-  inviteLink: string;  //Channel invite link
+  inviteLink: string; //Channel invite link
 };
 
-export enum Duration{
+export enum Duration {
   fifteenMinute = 0.25,
   oneHour = 60,
   threeHour = 180,
   eightHour = 480,
   twentyFourHour = 1440,
   manualTurnOn = 1,
-  manualTurnOff = 0
+  manualTurnOff = 0,
 }
