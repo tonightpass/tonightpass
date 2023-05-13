81a3b2ed6ba88022b61e8345ff5b4ee4eca4991d
Antoine Kingue <etorefr@gmail.com>
2023-05-13 16:35:16 +0200

    🏷️ update event ticket types

--- packages/shared-types/src/event/ticket/index.ts
+++ packages/shared-types/src/event/ticket/index.ts
@@ -1,11 +1,35 @@
+import { Currency } from "../..";
+
 export type EventTicket = {
   id: string;
-  title: string;
+  name: string;
+  description?: string;
   unitPrice: number;
-  feesIncluded: boolean;
   quantity: number;
+  type: EventTicketType;
+  category: EventTicketCategory;
+  currency: Currency;
+  vatRate: number;
+  externalId?: string;
+  isVisible: boolean;
+  isFeesIncluded: boolean;
   startAt: Date;
   endAt: Date;
   updatedAt: Date;
   createdAt: Date;
 };
+
+export type EventTicketType = "e-ticket" | "other";
+
+export enum EventTicketCategory {
+  ENTRY = "entry",
+  PACKAGE = "package",
+  MEAL = "meal",
+  DRINK = "drink",
+  PARKING = "parking",
+  ACCOMMODATION = "accommodation",
+  CAMPING = "camping",
+  LOCKER = "locker",
+  SHUTTLE = "shuttle",
+  OTHER = "other",
+}
