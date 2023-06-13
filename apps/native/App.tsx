670792dbb5fd77bd620a90094e1a36dc81d18fe8
Antoine Kingue <etorefr@gmail.com>
2023-06-14 01:03:16 +0200

    ✨ add workshop app

---
+++ apps/native/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style={"auto"} />
+      <Text style={styles.text}>{"Hello"}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 24,
+  }
+});
