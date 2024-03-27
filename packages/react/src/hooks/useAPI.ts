aafda6da7bafa214493096aec628581d7731880a
Antoine Kingue <contact@antoinek.fr>
2024-03-27 19:07:43 +0100

    ✨ add react sdk

---
+++ packages/react/src/hooks/useAPI.ts
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+import {
+  Endpoints,
+  APIRequestOptions,
+  Client,
+  PathsFor,
+  Query,
+} from "tonightpass";
+
+export const useAPI = <Path extends PathsFor<"GET">>(
+  path: Path,
+  query?: Query<Path>,
+  options?: APIRequestOptions,
+  client = new Client({ baseURL: "https://api.tonightpass.com" }),
+) => {
+  const fetcher = async () => {
+    return await client.get(path, query, options);
+  };
+
+  const swr = useSWR<Extract<Endpoints, { path: Path; method: "GET" }>["res"]>(
+    path,
+    fetcher,
+  );
+  return swr;
+};
