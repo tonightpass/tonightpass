c99849b54f0a966e0cd4fffc318c44b7d783e7cf
Antoine Kingue <contact@antoinek.fr>
2024-03-27 01:46:06 +0100

    ✅ add careers tests

--- packages/node/tests/careers/index.ts
+++ packages/node/tests/careers/index.ts
@@ -6,15 +6,11 @@ import { TonightPass } from "../../src/tonightpass";
 export function careersTests(tnp: TonightPass) {
   test("It gets all careers jobs", async () => {
     const careersJobs = await tnp.careers.jobs.getAll();
-    assert.ok(
-      Array.isArray(careersJobs.success ? careersJobs.data : undefined),
-    );
+    assert.ok(Array.isArray(careersJobs));
   });
 
   test("It gets all careers offices", async () => {
     const careersOffices = await tnp.careers.offices.getAll();
-    assert.ok(
-      Array.isArray(careersOffices.success ? careersOffices.data : undefined),
-    );
+    assert.ok(Array.isArray(careersOffices));
   });
 }
