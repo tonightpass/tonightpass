89180b70042d2599d08bf217ead1372f3502255f
Antoine Kingue <contact@antoinek.fr>
2024-03-28 13:55:23 +0100

    🔧 update tsup configs

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig, Options } from "tsup";
 
 const config: Options = {
+  format: ["cjs", "esm"],
   entry: ["src/index.ts"],
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
   platform: "node",
+  minify: true,
+  treeshake: true,
   dts: true,
 };
 
