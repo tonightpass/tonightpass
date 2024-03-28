1e2ddc3ed7ef6745b218f04567dcb0760eedddd9
Antoine Kingue <contact@antoinek.fr>
2024-03-28 14:20:45 +0100

    🩹 fix fetcher return function

--- packages/react/src/hooks/useAPI.ts
+++ packages/react/src/hooks/useAPI.ts
@@ -13,9 +13,8 @@ export const useAPI = <Path extends PathsFor<"GET">>(
   options?: APIRequestOptions,
   client = new Client({ baseURL: "https://api.tonightpass.com" }),
 ) => {
-  const fetcher = async () => {
-    return await client.get(path, query, options);
-  };
+  const fetcher = async (...args: Parameters<typeof fetch>) =>
+    client.get(args[0], query, options);
 
   return useSWR<Extract<Endpoints, { path: Path; method: "GET" }>["res"]>(
     path,
