dd0f40efcb510bd51dd0565992f9abedc64a2717
Antoine Kingue <contact@antoinek.fr>
2024-03-28 16:42:10 +0100

    👽️ update next app due to client changes

--- apps/next/src/pages/_app.tsx
+++ apps/next/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
+import { client } from "@tonightpass/react";
 import type { AppProps } from "next/app";
+
+client.setOptions({ baseURL: "http://localhost:8000" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
