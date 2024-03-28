843db754ef68df529a767e243e3784b40c1cd193
Antoine Kingue <contact@antoinek.fr>
2024-03-28 14:39:35 +0100

    🚨 fix lint errors

--- packages/react/src/hooks/useAPI.ts
+++ packages/react/src/hooks/useAPI.ts
@@ -13,8 +13,7 @@ export const useAPI = <Path extends PathsFor<"GET">>(
   options?: APIRequestOptions,
   client = new Client({ baseURL: "https://api.tonightpass.com" }),
 ) => {
-  const fetcher = async (...args: Parameters<typeof fetch>) =>
-    client.get(args[0], query, options);
+  const fetcher = (key: Path) => client.get(key, query, options);
 
   return useSWR<Extract<Endpoints, { path: Path; method: "GET" }>["res"]>(
     path,
