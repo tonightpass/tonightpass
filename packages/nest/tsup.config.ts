f147c24f063628c46be1c24859a8668c6df7b154
Antoine Kingue <contact@antoinek.fr>
2024-04-02 12:00:57 +0200

    ✨ add nest sdk

---
+++ packages/nest/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, Options } from "tsup";
+
+const config: Options = {
+  format: ["cjs", "esm"],
+  entry: ["src/index.ts"],
+  splitting: true,
+  sourcemap: true,
+  clean: false,
+  platform: "node",
+  minify: true,
+  treeshake: true,
+  dts: true,
+};
+
+export default defineConfig(config);
