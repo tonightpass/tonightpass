888a1958d3ece06261147d22e310d59063e42807
Antoine Kingue <contact@antoinek.fr>
2024-04-02 16:49:44 +0200

    🚨 fix lint errors in organizations dtos

--- packages/node/src/rest/dtos/organizations/update-organization.dto.ts
+++ packages/node/src/rest/dtos/organizations/update-organization.dto.ts
@@ -1,15 +1,3 @@
-import { IsNotEmpty, IsObject, IsString } from "class-validator";
-
 import { CreateOrganizationDto } from "./create-organization.dto";
-import { Location } from "../../types";
 
-export class UpdateOrganizationDto
-  implements Pick<CreateOrganizationDto, "name" | "location">
-{
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @IsObject()
-  location?: Location;
-}
+export class UpdateOrganizationDto extends CreateOrganizationDto {}
