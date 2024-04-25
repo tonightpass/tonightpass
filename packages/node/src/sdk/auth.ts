994066c83c282d9645894366557bcaa276b23904
Antoine Kingue <contact@antoinek.fr>
2024-04-25 11:42:22 +0200

    🎨 add oauth2 and org billing to sdk

--- packages/node/src/sdk/auth.ts
+++ packages/node/src/sdk/auth.ts
@@ -1,9 +1,33 @@
+import { ParamValue } from "pathcat";
+
 import { sdk } from "./builder";
 import { CreateUserDto, SignInUserDto } from "../rest";
+import { isBrowser } from "../utils";
 
 export const auth = sdk((client) => ({
   signIn: async (data: SignInUserDto) => client.post("/auth/sign-in", data),
   signUp: async (data: CreateUserDto) => client.post("/auth/sign-up", data),
   signOut: async () => client.post("/auth/sign-out", null),
   refreshToken: async () => client.post("/auth/refresh-token", null),
+
+  oauth2: {
+    google: {
+      connect: (params?: Record<string, ParamValue>) => {
+        if (isBrowser) {
+          window.location.href = client.url("/oauth2/google", params || {});
+        } else {
+          throw new Error("Google OAuth2 is only available in the browser");
+        }
+      },
+    },
+    twitter: {
+      connect: (params?: Record<string, ParamValue>) => {
+        if (isBrowser) {
+          window.location.href = client.url("/oauth2/twitter", params || {});
+        } else {
+          throw new Error("Twitter OAuth2 is only available in the browser");
+        }
+      },
+    },
+  },
 }));
