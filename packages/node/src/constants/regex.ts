e75b2eb68031599af222564956b4b02eac9bb0de
Antoine Kingue <contact@antoinek.fr>
2024-07-08 21:03:09 +0200

    🎨 improve regexes and tests units

--- packages/node/src/constants/regex.ts
+++ packages/node/src/constants/regex.ts
@@ -1,20 +1,23 @@
-export const EMAIL_REGEX = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-
-// checks if a password has at least one uppercase letter and a number or special character
-export const PASSWORD_REGEX =
-  /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-
-// checks if a string has only letters, numbers, spaces, apostrophes, dots and dashes
-export const NAME_REGEX = /(^[\p{L}\d'\\.\s\\-]*$)/u;
-
-// checks if a string is a valid slug, useful for usernames
-export const SLUG_REGEX = /^[a-z\d]+(?:(\.|-|_)[a-z\d]+)*$/;
-
-// validates if passwords are valid bcrypt hashes
-export const BCRYPT_HASH = /\$2[abxy]?\$\d{1,2}\$[A-Za-z\d\\./]{53}/;
-
-export const PHONE_NUMBER_REGEX =
-  /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/;
-
-export const IMAGE_URL_REGEX =
-  /(((http:\/\/www)|(http:\/\/)|(www))[-a-zA-Z0-9@:%_\\+.~#?&//=]+)\.(jpg|jpeg|gif|png|bmp|tiff|tga|svg)/i;
+export const REGEX = {
+  // matches a valid email address
+  EMAIL: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i,
+  // matches a name with only letters, numbers, spaces
+  NAME: /^[a-zA-Z0-9 ]+$/,
+  // matches a slug with only lowercase letters, numbers, dots and underscores
+  SLUG: /^[a-z0-9_.]+$/,
+  // matches an international phone number
+  PHONE: /^\+(?:[0-9] ?){6,14}[0-9]$/,
+  // matches a password with at least 8 characters, one uppercase letter, one lowercase letter and one number or special character
+  PASSWORD: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d\W]).{8,}$/,
+  // matches a password with at least 8 characters
+  PASSWORD_MIN_LENGTH: /^.{8,}$/,
+  // matches a password with one uppercase letter
+  PASSWORD_UPPERCASE: /^(?=.*[A-Z])/,
+  // matches a password with one lowercase letter
+  PASSWORD_LOWERCASE: /^(?=.*[a-z])/,
+  // matches a password with one number or special character
+  PASSWORD_NUMBER_SPECIAL: /^(?=.*[\d\W])/,
+  // matches an image url
+  IMAGE_URL:
+    /^(https:\/\/|http:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}([-a-zA-Z0-9@:%_\+.~#?&//=]*)\.(jpg|jpeg|gif|png|bmp|tiff|tga|svg)$/i,
+};
