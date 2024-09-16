ed0996da66461a2d92dff32d9d50a2a04a840e4d
Antoine Kingue <contact@antoinek.fr>
2024-09-16 13:19:40 +0200

    🏷️ add profiles relationships types

---
+++ .changeset/tender-forks-buy.md
@@ -0,0 +1,5 @@
+---
+"tonightpass": patch
+---
+
+Add profiles relationships types
--- packages/node/src/sdk/profiles.ts
+++
@@ -1,6 +0,0 @@
-import { sdk } from "./builder";
-
-export const profiles = sdk((client) => ({
-  get: async (username: string) =>
-    client.get("/profiles/:username", { username }),
-}));
