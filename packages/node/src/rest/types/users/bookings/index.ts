fdadaae14650f9b81477b33fdfd1fb06ed5ecfa9
Antoine Kingue <contact@antoinek.fr>
2024-10-20 21:31:05 +0200

    🏷️ update user booking endpoint types

--- packages/node/src/rest/types/users/bookings/index.ts
+++ packages/node/src/rest/types/users/bookings/index.ts
@@ -37,4 +37,11 @@ export type UserBookingEndpoints =
       ArrayResult<UserBooking>,
       ArrayOptions<UserBooking>
     >
-  | Endpoint<"GET", "/users/bookings/:bookingId", UserBooking>;
+  | Endpoint<
+      "GET",
+      "/users/:userId/bookings",
+      ArrayResult<UserBooking>,
+      ArrayOptions<UserBooking>
+    >
+  | Endpoint<"GET", "/users/bookings/:bookingId", UserBooking>
+  | Endpoint<"GET", "/users/@me/bookings/:bookingId", UserBooking>;
