eb08d5d1b59fb1878d513daf4bffb1d7836f12b8
Antoine Kingue <contact@antoinek.fr>
2024-10-22 00:07:32 +0200

    ♻️ refactor users suggestions into profiles suggestions

--- packages/node/src/sdk/profiles/index.ts
+++ packages/node/src/sdk/profiles/index.ts
@@ -1,8 +1,12 @@
 import { profilesRelationships } from "./relationships";
+import { ArrayOptions, OrganizationIdentity, UserIdentity } from "../../rest";
 import { sdk } from "../builder";
 
 export const profiles = sdk((client) => ({
   get: async (username: string) =>
     client.get("/profiles/:username", { username }),
+  getSuggestions: async (
+    options: ArrayOptions<UserIdentity | OrganizationIdentity>,
+  ) => client.get("/profiles/suggestions", options),
   relationships: profilesRelationships(client),
 }));
--- packages/node/src/sdk/users/index.ts
+++ packages/node/src/sdk/users/index.ts
@@ -3,7 +3,6 @@ import { sdk } from "../builder";
 
 export const users = sdk((client) => ({
   getAll: async () => client.get("/users"),
-  getSuggestions: async () => client.get("/users/suggestions"),
   get: async (userId: string) => client.get("/users/:userId", { userId }),
   me: async () => client.get("/users/@me"),
   check: async (identifier: string, suggestions?: boolean) =>
