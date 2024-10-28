93820d7c388a4a4568fbb3fe95f51b071b78c246
Antoine Kingue <contact@antoinek.fr>
2024-10-28 16:03:07 +0100

    ✨ add user file upload handling

--- packages/node/src/sdk/users/index.ts
+++ packages/node/src/sdk/users/index.ts
@@ -1,4 +1,5 @@
-import { UpdateUserDto } from "../../rest";
+import { UpdateUserDto, UserFileType } from "../../rest";
+import { buildFileFormData } from "../../utils";
 import { sdk } from "../builder";
 
 export const users = sdk((client) => ({
@@ -9,4 +10,21 @@ export const users = sdk((client) => ({
     client.get("/users/check/:identifier", { identifier, suggestions }),
   update: async (userId: string, data: UpdateUserDto) =>
     client.put("/users/:userId", data, { userId }),
+  uploadFile: async (
+    userId: string,
+    userFileType: UserFileType,
+    file: File,
+  ) => {
+    return client.post(
+      "/users/:userId/files/:userFileType",
+      undefined,
+      {
+        userId,
+        userFileType,
+      },
+      {
+        data: buildFileFormData("file", file),
+      },
+    );
+  },
 }));
