60d0c468ac8c031f2fdf577023e148e5268633e6
Antoine Kingue <etorefr@gmail.com>
2025-07-01 16:43:59 +0200

    🏷️ add organization-specific channel endpoints and enhance channel api structure

--- packages/node/src/sdk/channels/index.ts
+++ packages/node/src/sdk/channels/index.ts
@@ -1,28 +1,114 @@
-import { CreateChannelDto, UpdateChannelDto } from "../../rest";
+import {
+  CreateChannelDto,
+  UpdateChannelDto,
+  ArrayOptions,
+  Channel,
+  ChannelMember,
+  AddParticipantDto,
+  UserChannelCountOptions,
+} from "../../rest";
 import { sdk } from "../builder";
 import { channelsMessages } from "./messages";
 
 export const channels = sdk((client) => ({
-  getAll: async () => client.get("/channels"),
-  get: async (channelId: string) =>
-    client.get("/channels/:channelId", { channelId }),
-  create: async (data: CreateChannelDto) => client.post("/channels", data),
-  update: async (channelId: string, data: UpdateChannelDto) =>
-    client.put("/channels/:channelId", data, { channelId }),
-  delete: async (channelId: string) =>
-    client.delete("/channels/:channelId", undefined, { channelId }),
-  addParticipant: async (channelId: string, username: string) =>
-    client.post(
-      "/channels/:channelId/participants",
-      { username },
-      { channelId },
-    ),
-  removeParticipant: async (channelId: string, username: string) =>
-    client.delete("/channels/:channelId/participants/:username", undefined, {
-      channelId,
-      username,
+  me: async (options?: ArrayOptions<Channel>) =>
+    client.get("/channels/@me", options),
+  getByOrganization: async (
+    organizationSlug: string,
+    options?: ArrayOptions<Channel>,
+  ) =>
+    client.get("/channels/:organizationSlug", { organizationSlug, ...options }),
+  countMe: async (options?: UserChannelCountOptions) =>
+    client.get("/users/@me/channels/count", options),
+  countByOrganization: async (
+    organizationSlug: string,
+    options?: UserChannelCountOptions,
+  ) =>
+    client.get("/users/:organizationSlug/channels/count", {
+      organizationSlug,
+      ...options,
     }),
-  getMembers: async (channelId: string) =>
-    client.get("/channels/:channelId/members", { channelId }),
+  get: async (channelId: string) =>
+    client.get("/channels/@me/:channelId", { channelId }),
+  getByOrganizationChannel: async (
+    organizationSlug: string,
+    channelId: string,
+  ) =>
+    client.get("/channels/:organizationSlug/:channelId", {
+      organizationSlug,
+      channelId,
+    }),
+  create: async (data: CreateChannelDto) => client.post("/channels/@me", data),
+  createByOrganization: async (
+    organizationSlug: string,
+    data: CreateChannelDto,
+  ) => client.post("/channels/:organizationSlug", data, { organizationSlug }),
+  update: async (channelId: string, data: UpdateChannelDto) =>
+    client.put("/channels/@me/:channelId", data, { channelId }),
+  updateByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    data: UpdateChannelDto,
+  ) =>
+    client.put("/channels/:organizationSlug/:channelId", data, {
+      organizationSlug,
+      channelId,
+    }),
+  delete: async (channelId: string) =>
+    client.delete("/channels/@me/:channelId", undefined, { channelId }),
+  deleteByOrganization: async (organizationSlug: string, channelId: string) =>
+    client.delete("/channels/:organizationSlug/:channelId", undefined, {
+      organizationSlug,
+      channelId,
+    }),
+  addParticipant: async (channelId: string, data: AddParticipantDto) =>
+    client.post("/channels/@me/:channelId/participants", data, { channelId }),
+  addParticipantByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    data: AddParticipantDto,
+  ) =>
+    client.post("/channels/:organizationSlug/:channelId/participants", data, {
+      organizationSlug,
+      channelId,
+    }),
+  removeParticipant: async (channelId: string, username: string) =>
+    client.delete(
+      "/channels/@me/:channelId/participants/:username",
+      undefined,
+      {
+        channelId,
+        username,
+      },
+    ),
+  removeParticipantByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    username: string,
+  ) =>
+    client.delete(
+      "/channels/:organizationSlug/:channelId/participants/:username",
+      undefined,
+      {
+        organizationSlug,
+        channelId,
+        username,
+      },
+    ),
+  getMembers: async (
+    channelId: string,
+    options?: ArrayOptions<ChannelMember>,
+  ) =>
+    client.get("/channels/@me/:channelId/members", { channelId, ...options }),
+  getMembersByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    options?: ArrayOptions<ChannelMember>,
+  ) =>
+    client.get("/channels/:organizationSlug/:channelId/members", {
+      organizationSlug,
+      channelId,
+      ...options,
+    }),
   messages: channelsMessages(client),
 }));
--- packages/node/src/sdk/channels/messages/index.ts
+++ packages/node/src/sdk/channels/messages/index.ts
@@ -3,45 +3,119 @@ import {
   UpdateChannelMessageDto,
   ArrayOptions,
   ChannelMessage,
+  AddReactionDto,
 } from "../../../rest";
 import { sdk } from "../../builder";
 
 export const channelsMessages = sdk((client) => ({
   getAll: async (channelId: string, options?: ArrayOptions<ChannelMessage>) =>
-    client.get("/channels/:channelId/messages", {
+    client.get("/channels/@me/:channelId/messages", {
+      channelId,
+      ...options,
+    }),
+  getAllByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    options?: ArrayOptions<ChannelMessage>,
+  ) =>
+    client.get("/channels/:organizationSlug/:channelId/messages", {
+      organizationSlug,
       channelId,
       ...options,
     }),
   get: async (channelId: string, messageId: string) =>
-    client.get("/channels/:channelId/messages/:messageId", {
+    client.get("/channels/@me/:channelId/messages/:messageId", {
+      channelId,
+      messageId,
+    }),
+  getByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    messageId: string,
+  ) =>
+    client.get("/channels/:organizationSlug/:channelId/messages/:messageId", {
+      organizationSlug,
       channelId,
       messageId,
     }),
   create: async (channelId: string, data: CreateChannelMessageDto) =>
-    client.post("/channels/:channelId/messages", data, { channelId }),
+    client.post("/channels/@me/:channelId/messages", data, { channelId }),
+  createByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    data: CreateChannelMessageDto,
+  ) =>
+    client.post("/channels/:organizationSlug/:channelId/messages", data, {
+      organizationSlug,
+      channelId,
+    }),
   update: async (
     channelId: string,
     messageId: string,
     data: UpdateChannelMessageDto,
   ) =>
-    client.put("/channels/:channelId/messages/:messageId", data, {
+    client.put("/channels/@me/:channelId/messages/:messageId", data, {
       channelId,
       messageId,
     }),
+  updateByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    messageId: string,
+    data: UpdateChannelMessageDto,
+  ) =>
+    client.put(
+      "/channels/:organizationSlug/:channelId/messages/:messageId",
+      data,
+      {
+        organizationSlug,
+        channelId,
+        messageId,
+      },
+    ),
   delete: async (channelId: string, messageId: string) =>
-    client.delete("/channels/:channelId/messages/:messageId", undefined, {
+    client.delete("/channels/@me/:channelId/messages/:messageId", undefined, {
       channelId,
       messageId,
     }),
-  addReaction: async (channelId: string, messageId: string, emoji: string) =>
+  deleteByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    messageId: string,
+  ) =>
+    client.delete(
+      "/channels/:organizationSlug/:channelId/messages/:messageId",
+      undefined,
+      {
+        organizationSlug,
+        channelId,
+        messageId,
+      },
+    ),
+  addReaction: async (
+    channelId: string,
+    messageId: string,
+    data: AddReactionDto,
+  ) =>
     client.post(
-      "/channels/:channelId/messages/:messageId/reactions",
-      { emoji },
+      "/channels/@me/:channelId/messages/:messageId/reactions",
+      data,
       { channelId, messageId },
+    ),
+  addReactionByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    messageId: string,
+    data: AddReactionDto,
+  ) =>
+    client.post(
+      "/channels/:organizationSlug/:channelId/messages/:messageId/reactions",
+      data,
+      { organizationSlug, channelId, messageId },
     ),
   removeReaction: async (channelId: string, messageId: string, emoji: string) =>
     client.delete(
-      "/channels/:channelId/messages/:messageId/reactions/:emoji",
+      "/channels/@me/:channelId/messages/:messageId/reactions/:emoji",
       undefined,
       {
         channelId,
@@ -49,9 +123,39 @@ export const channelsMessages = sdk((client) => ({
         emoji,
       },
     ),
+  removeReactionByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    messageId: string,
+    emoji: string,
+  ) =>
+    client.delete(
+      "/channels/:organizationSlug/:channelId/messages/:messageId/reactions/:emoji",
+      undefined,
+      {
+        organizationSlug,
+        channelId,
+        messageId,
+        emoji,
+      },
+    ),
   markAsRead: async (channelId: string, messageId: string) =>
-    client.post("/channels/:channelId/messages/:messageId/read", null, {
+    client.post("/channels/@me/:channelId/messages/:messageId/read", null, {
       channelId,
       messageId,
     }),
+  markAsReadByOrganization: async (
+    organizationSlug: string,
+    channelId: string,
+    messageId: string,
+  ) =>
+    client.post(
+      "/channels/:organizationSlug/:channelId/messages/:messageId/read",
+      null,
+      {
+        organizationSlug,
+        channelId,
+        messageId,
+      },
+    ),
 }));
