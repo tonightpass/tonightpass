a32937e1ff15e31d4084a00fe7aab61586e4ecba
Antoine Kingue <etorefr@gmail.com>
2025-07-15 02:16:52 +0200

    🏷️ add currencies sdk methods and reorganize currency types

---
+++ .changeset/reorganize-currency-types.md
@@ -0,0 +1,9 @@
+---
+"tonightpass": patch
+---
+
+Reorganize Currency enum to currency module
+
+- Move Currency enum from main types to currency/index.ts
+- Keep Currency enum co-located with currency-related interfaces
+- Improve module organization and type structure
---
+++ packages/node/src/rest/types/currencies/index.ts
@@ -0,0 +1,63 @@
+import { Endpoint } from "../../endpoints";
+
+// Currency
+export enum Currency {
+  // Major currencies
+  EUR = "EUR",
+  USD = "USD",
+  GBP = "GBP",
+
+  // European currencies (EU member states)
+  BGN = "BGN",
+  CZK = "CZK",
+  DKK = "DKK",
+  HUF = "HUF",
+  PLN = "PLN",
+  RON = "RON",
+  SEK = "SEK",
+
+  // European currencies (non-EU but European)
+  CHF = "CHF",
+  NOK = "NOK",
+  ISK = "ISK",
+  TRY = "TRY",
+  RUB = "RUB",
+  UAH = "UAH",
+  BAM = "BAM",
+  MKD = "MKD",
+  ALL = "ALL",
+  RSD = "RSD",
+  MDL = "MDL",
+  GEL = "GEL",
+  BYN = "BYN",
+}
+
+export interface ExchangeRates {
+  base: Currency.EUR;
+  rates: Record<Currency, number>;
+  updatedAt: Date;
+}
+
+export interface CurrencyConversion {
+  from: Currency;
+  to: Currency;
+  amount: number;
+}
+
+export interface CurrencyConversionResult {
+  originalAmount: number;
+  originalCurrency: Currency;
+  convertedAmount: number;
+  targetCurrency: Currency;
+  exchangeRate: number;
+  convertedAt: Date;
+}
+
+export type CurrenciesEndpoints =
+  | Endpoint<"GET", "/currencies/rates", ExchangeRates>
+  | Endpoint<
+      "POST",
+      "/currencies/convert",
+      CurrencyConversionResult,
+      CurrencyConversion
+    >;
