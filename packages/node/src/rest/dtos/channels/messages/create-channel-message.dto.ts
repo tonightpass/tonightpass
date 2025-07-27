7ecfcb1254d0fbb134a7a790a98023c3b280bf74
Antoine Kingue <etorefr@gmail.com>
2025-07-27 02:58:47 +0200

    🦺 update messages with only attachments to be allowed by updating validation logic

--- packages/node/src/rest/dtos/channels/messages/create-channel-message.dto.ts
+++ packages/node/src/rest/dtos/channels/messages/create-channel-message.dto.ts
@@ -6,11 +6,37 @@ import {
   ArrayMaxSize,
   IsMongoId,
   Matches,
+  ValidateIf,
+  Validate,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
 } from "class-validator";
+
+@ValidatorConstraint({ name: "contentOrAttachments", async: false })
+export class ContentOrAttachmentsConstraint
+  implements ValidatorConstraintInterface
+{
+  validate(_value: unknown, args: ValidationArguments): boolean {
+    const object = args.object as CreateChannelMessageDto;
+    const hasContent = Boolean(
+      object.content && object.content.trim().length > 0,
+    );
+    const hasAttachments = Boolean(
+      object.attachments && object.attachments.length > 0,
+    );
+    return hasContent || hasAttachments;
+  }
+
+  defaultMessage(_args: ValidationArguments): string {
+    return "Either content or attachments must be provided";
+  }
+}
 
 export class CreateChannelMessageDto {
   @IsOptional()
   @IsString()
+  @ValidateIf((o) => o.content !== undefined)
   @Length(1, 4000)
   content?: string;
 
@@ -20,6 +46,7 @@ export class CreateChannelMessageDto {
   @Matches(/^channels\/[\w-]+\/messages\/[\w-]+\/private\/[\w-]+$/, {
     each: true,
   })
+  @Validate(ContentOrAttachmentsConstraint)
   attachments?: string[];
 
   @IsOptional()
