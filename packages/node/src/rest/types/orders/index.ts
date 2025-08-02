25f444bcfffdad19d1d1e81913e9b91bc1428621
Antoine Kingue <etorefr@gmail.com>
2025-08-02 21:55:38 +0200

    🏷️ update order to use native stripe-invoice types

--- packages/node/src/rest/types/orders/index.ts
+++ packages/node/src/rest/types/orders/index.ts
@@ -4,14 +4,8 @@ import { ArrayOptions, ArrayResult, Base } from "..";
 import { Endpoint } from "../../endpoints";
 import { User } from "../users";
 
-export type Invoice = Stripe.Response<Stripe.Invoice> & {
-  payment_intent?: string | Stripe.PaymentIntent;
-  client_secret?: string;
-  confirmation_secret?: string;
-};
-
 export type Order = Base & {
-  invoice: Invoice;
+  invoice: Stripe.Invoice;
   user: User;
 };
 
