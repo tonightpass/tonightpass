33a56dfa473a7efca10c4cc6679eb68fe3f1f25d
Antoine Kingue <etorefr@gmail.com>
2025-08-02 19:52:42 +0200

    🏷️ add invoice type for stripe connect compatibility

--- packages/node/src/rest/types/orders/index.ts
+++ packages/node/src/rest/types/orders/index.ts
@@ -4,8 +4,14 @@ import { ArrayOptions, ArrayResult, Base } from "..";
 import { Endpoint } from "../../endpoints";
 import { User } from "../users";
 
+export type Invoice = Stripe.Response<Stripe.Invoice> & {
+  payment_intent?: string | Stripe.PaymentIntent;
+  client_secret?: string;
+  confirmation_secret?: string;
+};
+
 export type Order = Base & {
-  invoice: Stripe.Invoice;
+  invoice: Invoice;
   user: User;
 };
 
