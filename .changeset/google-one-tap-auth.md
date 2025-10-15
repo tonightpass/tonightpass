9330cc84150bec31f61a0cc11f1fd177fe57da90
Antoine Kingue <etorefr@gmail.com>
2025-10-16 00:55:56 +0200

    🏷️ add google one tap

---
+++ .changeset/google-one-tap-auth.md
@@ -0,0 +1,5 @@
+---
+"tonightpass": patch
+---
+
+Add Google One Tap authentication support with new `/auth/google-one-tap` endpoint and `GoogleOneTapDto`
--- packages/node/src/rest/dtos/users/index.ts
+++ packages/node/src/rest/dtos/users/index.ts
@@ -1,4 +1,5 @@
 export * from "./create-user.dto";
+export * from "./google-one-tap.dto";
 export * from "./recovery.dto";
 export * from "./recovery-reset.dto";
 export * from "./sign-in-user.dto";
