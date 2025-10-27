e48e0064a3903651161f7f4ba5f3b56834fe1d59
Antoine Kingue <etorefr@gmail.com>
2025-10-28 00:49:40 +0100

    ♻️ refactor booking tickets structure for improved code organization

---
+++ packages/node/src/sdk/users/bookings/tickets/index.ts
@@ -0,0 +1,15 @@
+import { sdk } from "../../../builder";
+
+export const usersBookingsTickets = sdk((client) => ({
+  get: async (ticketId: string) =>
+    client.get("/users/bookings/tickets/:ticketId", { ticketId }),
+  use: async (ticketId: string, tokenId: string, tokenValue: string) =>
+    client.put(
+      "/users/bookings/tickets/:ticketId/use",
+      {
+        tokenId,
+        tokenValue,
+      },
+      { ticketId },
+    ),
+}));
