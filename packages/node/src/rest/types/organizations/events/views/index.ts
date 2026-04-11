3640c5ee359b85ff35ada1a9683d408c37904ee4
Antoine Kingue <etorefr@gmail.com>
2026-04-11 20:41:42 +0200

    ✨ add event views sse stream endpoint, sdk method and useApiSubscription hook

--- packages/node/src/rest/types/organizations/events/views/index.ts
+++ packages/node/src/rest/types/organizations/events/views/index.ts
@@ -1,8 +1,24 @@
 import type { Endpoint } from "../../../../endpoints";
 
-export type OrganizationEventViewEndpoints = Endpoint<
-  "POST",
-  "/organizations/@:organizationSlug/events/:eventSlug/views",
-  boolean,
-  null
->;
+export type OrganizationEventViewOptions = {
+  events: string | string[];
+};
+
+export type OrganizationEventViewResult = {
+  eventId: string;
+  viewsCount: number;
+};
+
+export type OrganizationEventViewEndpoints =
+  | Endpoint<
+      "POST",
+      "/organizations/@:organizationSlug/events/:eventSlug/views",
+      boolean,
+      null
+    >
+  | Endpoint<
+      "GET",
+      "/organizations/events/views/stream",
+      ReadableStream<OrganizationEventViewResult>,
+      OrganizationEventViewOptions
+    >;
--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./use-api";
 export * from "./use-api-infinite";
+export * from "./use-api-subscription";
 export * from "./use-web-socket";
