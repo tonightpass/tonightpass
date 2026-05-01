c8cc0f83a2b5b449a3dacb0170fc98484315e9e0
Antoine Kingue <etorefr@gmail.com>
2026-05-01 15:07:45 +0200

    ✨ add post views stream endpoint

--- packages/node/src/rest/types/users/posts/views/index.ts
+++ packages/node/src/rest/types/users/posts/views/index.ts
@@ -1,8 +1,19 @@
 import type { Endpoint } from "../../../../endpoints";
 
-export type UserPostViewEndpoints = Endpoint<
-  "POST",
-  "/users/@:username/posts/:postId/views",
-  boolean,
-  null
->;
+export type UserPostViewOptions = {
+  posts: string | string[];
+};
+
+export type UserPostViewResult = {
+  postId: string;
+  viewsCount: number;
+};
+
+export type UserPostViewEndpoints =
+  | Endpoint<"POST", "/users/@:username/posts/:postId/views", boolean, null>
+  | Endpoint<
+      "GET",
+      "/users/posts/views/stream",
+      ReadableStream<UserPostViewResult>,
+      UserPostViewOptions
+    >;
