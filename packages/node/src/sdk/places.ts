dda4221222b9fc1fffbb1f43bf26aefa113fe266
Antoine Kingue <etorefr@gmail.com>
2026-05-14 20:19:51 +0200

    ♻️ refactor places sdk to query pattern

--- packages/node/src/sdk/places.ts
+++ packages/node/src/sdk/places.ts
@@ -1,44 +1,32 @@
-import type {
-  ArrayOptions,
-  NearbyCitiesOptions,
-  PlaceCity,
-  PlaceCountry,
-} from "../rest";
+import type { Query } from "pathcat";
+
 import { sdk } from "./builder";
 
 export const places = sdk((client) => ({
   countries: {
-    getAll: async (options?: ArrayOptions<PlaceCountry>) =>
-      client.get("/places/countries", options),
-    get: async (countrySlug: string) =>
-      client.get("/places/countries/:countrySlug", { countrySlug }),
+    getAll: async (query?: Query<"/places/countries">) =>
+      client.get("/places/countries", query),
+    get: async (query: Query<"/places/countries/:countrySlug">) =>
+      client.get("/places/countries/:countrySlug", query),
     cities: {
-      getAll: async (countrySlug: string, options?: ArrayOptions<PlaceCity>) =>
-        client.get("/places/countries/:countrySlug/cities", {
-          countrySlug,
-          ...options,
-        }),
-      get: async (countrySlug: string, citySlug: string) =>
-        client.get("/places/countries/:countrySlug/cities/:citySlug", {
-          countrySlug,
-          citySlug,
-        }),
+      getAll: async (query: Query<"/places/countries/:countrySlug/cities">) =>
+        client.get("/places/countries/:countrySlug/cities", query),
+      get: async (
+        query: Query<"/places/countries/:countrySlug/cities/:citySlug">
+      ) => client.get("/places/countries/:countrySlug/cities/:citySlug", query),
       nearby: async (
-        countrySlug: string,
-        citySlug: string,
-        options?: NearbyCitiesOptions
+        query: Query<"/places/countries/:countrySlug/cities/:citySlug/nearby">
       ) =>
-        client.get("/places/countries/:countrySlug/cities/:citySlug/nearby", {
-          countrySlug,
-          citySlug,
-          ...options,
-        }),
+        client.get(
+          "/places/countries/:countrySlug/cities/:citySlug/nearby",
+          query
+        ),
     },
   },
   cities: {
-    getAll: async (options?: ArrayOptions<PlaceCity>) =>
-      client.get("/places/cities", options),
-    search: async (query: string, options?: ArrayOptions<PlaceCity>) =>
-      client.get("/places/cities/search", { q: query, ...options }),
+    getAll: async (query?: Query<"/places/cities">) =>
+      client.get("/places/cities", query),
+    search: async (query: Query<"/places/cities/search">) =>
+      client.get("/places/cities/search", query),
   },
 }));
